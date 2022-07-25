f2086d48e0ce7e3980b50531ab7658eafbd40540
patrickbasham <PatrickTBasham@gmail.com>
2022-07-25 14:13:19 -0400

    setup database.js connection and file

---
+++ config/database.js
@@ -0,0 +1,35 @@
+/*========================================
+        Import Dependencies
+========================================*/
+const mongoose = require("mongoose")
+
+/*========================================
+        Database Conection
+========================================*/
+const db = mongoose.connection
+
+/*========================================
+        Connection to Local MongoDB & MongoAtlas
+========================================*/
+const MONGODB_URI = process.env.MONGODB_URI
+
+const DATABASE_URL = process.env.DATABASE_URL
+
+const CONFIG = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
+
+mongoose.connect(DATABASE_URL, CONFIG)
+db.on("connected", () => {
+    console.log(`Connected to ${db.name} at ${db.host}: ${db.port}`)
+})
+db.on("close", () => {
+    console.log(`Disconnected from ${db.host} at ${db.host}: ${db.port}`)
+})
+.on("error", (error) => console.log(error))
+
+/*========================================
+        Export Modules
+========================================*/
+module.exports = mongoose
