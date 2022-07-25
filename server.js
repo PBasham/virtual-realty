770e7da0dbe22f90f84a6c0df25d6af1233f49d1
patrickbasham <PatrickTBasham@gmail.com>
2022-07-25 13:37:11 -0400

    added some middleware to server.js

--- server.js
+++ server.js
@@ -9,3 +9,17 @@ const logger = require("morgan")
 // connect to dotenv file and connect to database file.
 require("dotenv").config()
 require("./config/database.js")
+
+/*========================================
+        Middleware
+========================================*/
+const app = express()
+
+app.use(logger("dev"))
+app.use(express.json)
+
+app.use(favicon(path.join(__dirname, "build", "favicon.ico")))
+app.use(express.static(path.join(__dirname, "build")))
+
+// Middleware that will verify the token and assign a user object of patload to req.user (must come before routes)
+// app.use(require("./config/checkToken"))
