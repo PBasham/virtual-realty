4c3e86297e6ef96920183d36a1fa36202d8aaa72
patrickbasham <PatrickTBasham@gmail.com>
2022-07-26 18:19:18 -0400

    added base export for pages and fixed type on users-services

--- src/utilities/users-services.js
+++ src/utilities/users-services.js
@@ -31,7 +31,7 @@ export function getToken() {
     const token = localStorage.getItem("token")
     if (!token) return null
     const payload = JSON.parse(window.atob(token.split(".")[1]))
-    if (payload.exp < DataTransfer.now() / 1000) {
+    if (payload.exp < Date.now() / 1000) {
         localStorage.removeItem("token")
         return null
     }
