99240d6a1f92d87c03656af9e29bf7c55b949042
patrickbasham <PatrickTBasham@gmail.com>
2022-07-26 18:00:31 -0400

    added component for WatchListPage and setup state for object

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,7 +2,7 @@
         Import Dependencies
 ========================================*/
 import { useState, useEffect, useRef } from "react"
-import { Routes, Route, Navigate } from "react-router-dom"
+import { Routes, Route, Navigate, Redirect } from "react-router-dom"
 import { getUser } from "../../utilities/users-services.js"
 
 // import logo from './logo.svg';
@@ -12,20 +12,72 @@ import { getUser } from "../../utilities/users-services.js"
 import './App.css'
 
 function App() {
+    const [user, setUset] = useState(getUser())
+    const [navBarLinks, setNavBarLinks] = useState({
+        activeNavLink: null,
+        navLinks: [
+            {
+                id: 0,
+                name: "Home",
+                to: "/home",
+                element: <HomePage />,
+                subElement: [],
+            },
+            {
+                id: 1,
+                name: "About",
+                to: "/about",
+                element: <AboutPage />,
+                subElement: {
+                    activeSubElement: null,
+                    subElements: [
+                        {
+                            id: 0,
+                            name: "Company",
+                            href: "#company",
+                        },
+                        {
+                            id: 1,
+                            name: "Team",
+                            href: "#team",
+                        },
+                        {
+                            id: 2,
+                            name: "Services",
+                            href: "#services",
+                        },
+                    ]
+                }
+            },
+            {
+                id: 2,
+                name: "Watchlist",
+                to: "/watchlist",
+                element: <WatchListPage />,
+                subElement: [],
+            },
+            {
+                id: 3,
+                name: "Explore",
+                to: "/explore",
+                element: <ExplorePage />,
+                subElement: [],
+            },
+        ]
+    })
 
-    
-    
+
     /*========================================
             Functins
     ========================================*/
 
-    
+
     //==-- END FUNCTIONS--==//
-  return (
-    <div className="App">
-        Testing
-    </div>
-  );
+    return (
+        <div className="App">
+            Testing
+        </div>
+    );
 }
 
 export default App;
