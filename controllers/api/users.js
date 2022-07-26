6eeaea99f243af538b5ac198c1d093d294337af6
patrickbasham <PatrickTBasham@gmail.com>
2022-07-26 12:44:40 -0400

    added login funciton

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -19,3 +19,16 @@ function checkToken(req, res) {
     console.log("req.user:", req.user)
     res.json(req.exp)
 }
+
+async function login( req, res) {
+    try {
+        const user = await User.findOne({ email: req.body.email })
+        console.log("Req.Body: ", req.body)
+        if (!user) throw new Error()
+        const match = await bcrypt.compate(req.body.password, user.password)
+        if (!match) throw new Error()
+        res.json(createJWT(user))
+    } catch {
+        res.status(400).json("Invalid Credentials")
+    }
+}
