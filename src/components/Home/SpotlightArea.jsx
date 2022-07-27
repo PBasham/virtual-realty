82cfa94ca515472a2eaf69bc4a845b63b8ae93a4
patrickbasham <PatrickTBasham@gmail.com>
2022-07-27 14:18:58 -0400

    set up styling and positioning of spotlight area

--- src/components/Home/SpotlightArea.jsx
+++ src/components/Home/SpotlightArea.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function SpotlightArea() {
     return (
         <div className="spotlight-div">
-            <div className="preview-main">Main Preview</div>
+            <div className="preview-main">
+                <div className="preview-title">Preview Title Name</div>
+                Image-Of-House
+            </div>
             <div className="preview-list">
                 <div className="preview-list-item">List Items will go here</div>
                 <div className="preview-list-item">List Items will go here</div>
