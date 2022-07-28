49cde06c2f146da4cecf7e56aa0ab4c097cdb31b
patrickbasham <PatrickTBasham@gmail.com>
2022-07-28 10:27:25 -0400

    added routes file and index route for listings

---
+++ routes/api/listings.js
@@ -0,0 +1,20 @@
+/*========================================
+        Import Dependencies
+========================================*/
+const express = require("express")
+const router = express.Router()
+const listingsCtrl = require("../../controllers/api/listings.js")
+
+/*========================================
+        Routes
+========================================*/
+// Index Route
+router.get("/". listingsCtrl.index)
+// Show Route
+
+// Search Route
+
+/*========================================
+        Exports
+========================================*/
+module.exports = router
