80dc1d684ed93e3b17f706ed711dd0f99e03f433
patrickbasham <PatrickTBasham@gmail.com>
2022-07-29 02:48:41 -0400

    setup routes for watchlist

--- src/utilities/watchlist-api.js
+++ src/utilities/watchlist-api.js
@@ -1,6 +1,16 @@
 /*========================================
         Import Dependencies
 ========================================*/
+import { base } from "../../models/user";
 import sendRequest from "./send-request";
 
 const BASE_URL = "/api/watchlist"
+// index route
+export function getUserWatchList() {
+    return sendRequest(BASE_URL)
+}
+
+// show route
+export function getWatchListById(id) {
+    return sendRequest(`${BASE_URL}/${id}`)
+}
