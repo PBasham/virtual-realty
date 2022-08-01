1c1fea5ca990351073034f778ce2246668c578a9
patrickbasham <PatrickTBasham@gmail.com>
2022-08-01 15:26:04 -0400

    set imgs for homepage service cards

--- src/components/Home/ServiceCards.jsx
+++ src/components/Home/ServiceCards.jsx
@@ -7,17 +7,17 @@ export default function ServiceCards() {
     const serviceCards = [
         {
             title: "Buy",
-            image: "",
+            img: "https://blog.rossmortgage.com/wp-content/uploads/2017/03/best-online-home-buying-tools.jpg",
             miniDesc: "Browse through beautiful homes to buy.",
         },
         {
             title: "Sell",
-            image: "",
+            img: "https://www.gannett-cdn.com/-mm-/8fd3f2c5eeb4f432937d634a62b8f18185a92e99/c=0-125-2097-1305/local/-/media/2019/03/07/USATODAY/usatsports/MotleyFool-TMOT-caeb51db-sold-sign.jpg",
             miniDesc: "Sell dat house!",
         },
         {
             title: "Concierge",
-            image: "",
+            img: "https://www.sinergicaservizi.it/wp-content/uploads/revslider/home-11/bell-concierge.jpg",
             miniDesc: "This is a service we offer!",
         },
     ]
