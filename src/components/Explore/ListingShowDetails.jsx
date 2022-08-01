040c97cb072852bb94dcb694ed4376575fe088f3
patrickbasham <PatrickTBasham@gmail.com>
2022-08-01 01:17:16 -0400

    added monthly estimator with styling and styled year options

--- src/components/Explore/ListingShowDetails.jsx
+++ src/components/Explore/ListingShowDetails.jsx
@@ -6,9 +6,9 @@ export default function ListingShowDetails({ price, priceDispaly }) {
 
     const calcMonthlyPrice = (months) => {
         const monthlyPayment = (price.replace(/\,/g,'') / months).toFixed(2)
-        return monthlyPayment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        return `$${monthlyPayment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`
     }
-    const [ monthlyCalc, setMonthlyCalc ] = useState("")
+    const [ monthlyCalc, setMonthlyCalc ] = useState("(Select a year for an estimate)")
 
 
     const handleYearChange = (e) => {
@@ -21,28 +21,29 @@ export default function ListingShowDetails({ price, priceDispaly }) {
         <div className="listing-details-wrapper">
             <div className="listing-details-main">
                 <h2>For Sale</h2>
-                <h1>Price {price}</h1>
+                <h1>Price ${price}</h1>
+                <h2 className="flex-hor-center">Monthly Est. Payment</h2>
                 <div className="listing-detail-estimate-options">
                     <input id="radio-10" type="radio" value="10" name="years" onClick={handleYearChange} />
-                    <label htmlFor="radio-10" value="10" >10
+                    <label htmlFor="radio-10" value="10" >10yr
                     </label>
                     <input id="radio-15" type="radio" value="15" name="years" onClick={handleYearChange} />
-                    <label htmlFor="radio-15" >15
+                    <label htmlFor="radio-15" >15yr
                     </label>
                     <input id="radio-20" type="radio" value="20" name="years" onClick={handleYearChange} />
-                    <label htmlFor="radio-20" >20
+                    <label htmlFor="radio-20" >20yr
                     </label>
                     <input id="radio-25" type="radio" value="25" name="years" onClick={handleYearChange} />
-                    <label htmlFor="radio-25" >25
+                    <label htmlFor="radio-25" >25yr
                     </label>
                     <input id="radio-30" type="radio" value="30" name="years" onClick={handleYearChange} />
-                    <label htmlFor="radio-30" >30
+                    <label htmlFor="radio-30" >30yr
                     </label>
                 </div>
-                <h2 className="listing-detail-estimate-calc">${monthlyCalc}</h2>
+                <h2 className="listing-detail-estimate-calc">{monthlyCalc}</h2>
             </div>
             <div className="listing-details-other">
-                
+
             </div>
             <div className="listing-details-tags">1</div>
         </div>
