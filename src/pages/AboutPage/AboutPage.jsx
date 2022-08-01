bbaabcbc8bfcde2b30f7e654b55b50dad34ccfcc
patrickbasham <PatrickTBasham@gmail.com>
2022-08-01 12:40:34 -0400

    setup base for about page

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+/*========================================
+        Import Styles
+========================================*/
+import "./AboutPage.css"
 
 export default function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <div className="about-wrapper">
+        <div className="mission-wrapper">
+            <h2>Our Mission</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut pharetra sit amet aliquam. Ipsum dolor sit amet consectetur adipiscing elit. Eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque. Dignissim convallis aenean et tortor.</p>
+        </div>
+    </div>
   )
 }
