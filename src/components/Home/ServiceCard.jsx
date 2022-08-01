9835ebf9790bcf2ed692a5bfcc3328f9c4c1bb3f
patrickbasham <PatrickTBasham@gmail.com>
2022-08-01 16:59:53 -0400

    setup service cards on homepage

--- src/components/Home/ServiceCard.jsx
+++ src/components/Home/ServiceCard.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 export default function ServiceCard({ title, img, desc }) {
     return (
-        <div className="service-cards-item">
-            <div className="service-card-img">
-                <img src={img} alt="" />
-            </div>
+        <div className="service-card">
             <div className="service-card-desc">
                 <p>{desc}</p>
+                <a href="/about">
+                    Click To Learn More
+                </a>
+            </div>
+            <div className="service-card-img">
+                <img src={img} alt="" />
             </div>
             <h3 className="service-card-title">
                 {title}
--- src/components/Home/ServiceCards.jsx
+++ src/components/Home/ServiceCards.jsx
@@ -1,38 +1,37 @@
 /*========================================
         Import Components
 ========================================*/
+import { useState } from "react"
 import ServiceCard from "./ServiceCard"
 
 export default function ServiceCards() {
-    const serviceCards = [
+    const [serviceCards, setServiceCards] = useState([
         {
             title: "Buy",
             img: "https://blog.rossmortgage.com/wp-content/uploads/2017/03/best-online-home-buying-tools.jpg",
-            miniDesc: "Browse through beautiful homes to buy.",
+            miniDesc: "Want to find your new home and reduce the hassle?",
         },
         {
             title: "Sell",
             img: "https://www.gannett-cdn.com/-mm-/8fd3f2c5eeb4f432937d634a62b8f18185a92e99/c=0-125-2097-1305/local/-/media/2019/03/07/USATODAY/usatsports/MotleyFool-TMOT-caeb51db-sold-sign.jpg",
-            miniDesc: "Sell dat house!",
+            miniDesc: "Selling your home can be stressful. We make it seamless.",
         },
         {
             title: "Concierge",
             img: "https://www.sinergicaservizi.it/wp-content/uploads/revslider/home-11/bell-concierge.jpg",
-            miniDesc: "This is a service we offer!",
+            miniDesc: "Finding your new home with a white glove experience",
         },
-    ]
+    ])
     return (
-        <div className="content-div">
-            <div className="service-cards">
-                {serviceCards.map((service, index) => (
-                    <ServiceCard
-                        key={index}
-                        title={service.title}
-                        img={service.image}
-                        desc={service.miniDesc}
-                    />
-                ))}
-            </div>
+        <div className="service-cards">
+            {serviceCards.map((service, index) => (
+                <ServiceCard
+                    key={index}
+                    title={service.title}
+                    img={service.img}
+                    desc={service.miniDesc}
+                />
+            ))}
         </div>
     )
 }
