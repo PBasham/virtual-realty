9f90324b4ac19da6f0ab4b97972bcb9184ca17aa
patrickbasham <PatrickTBasham@gmail.com>
2022-08-02 04:36:38 -0400

    updated data for team members

--- src/components/About/Mission.jsx
+++ src/components/About/Mission.jsx
@@ -4,7 +4,9 @@ export default function Mission() {
     return (
         <div className="mission-wrapper">
             <h2>Our Mission</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut pharetra sit amet aliquam. Ipsum dolor sit amet consectetur adipiscing elit. Eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque. Dignissim convallis aenean et tortor.</p>
+                <p>Let’s be honest, buying and selling homes are like an on-site job. Going from location to location, chatting with various home professionals, banks, and the list goes on. With the help of our expert professionals and services, we cut out the stress to make it more of a bliss. 
+By providing the services you need, you get the best part of the experience without having to leave the comfort of your home.
+</p>
         </div>
     )
 }
