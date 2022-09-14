552175689a23b6f35d93159e80f8b61cba4572df
patrickbasham <PatrickTBasham@gmail.com>
2022-09-14 19:19:39 -0400

    added responsivness to MyProfile page for mobile

--- src/components/UserProfile/UserUpdateEmail.jsx
+++ src/components/UserProfile/UserUpdateEmail.jsx
@@ -1,29 +1,31 @@
-export default function UserUpdateEmail({ handleEmailUpdate, allowEdit, verifyEmail, handleEmailChange, disableEmailBtn, tempUserData, adminAccount,}) {
+export default function UserUpdateEmail({ handleEmailUpdate, allowEdit, verifyEmail, handleEmailChange, disableEmailBtn, tempUserData, adminAccount, }) {
     return (
         <form onSubmit={handleEmailUpdate}>
-            <h4>Email: {tempUserData.email}</h4>
-            <label>New Email:
-            </label>
-            <input
-                disabled={adminAccount ? true : !allowEdit}
-                type="email"
-                value={adminAccount ? "Cannot Edit Test User" : verifyEmail.email}
-                name="email"
-                onChange={handleEmailChange}
-            />
-            <label>Confirm Email:
-            </label>
-            <input
-                disabled={adminAccount ? true : !allowEdit}
-                type="email"
-                value={adminAccount ? "Cannot Edit Test User" : verifyEmail.confirmEmail}
-                name="confirmEmail"
-                onChange={handleEmailChange}
-            />
-            <button
-                className="btn no-margin-left"
-                disabled={disableEmailBtn}
-            >Update</button>
+            <fieldset>
+                <h4>Email: {tempUserData.email}</h4>
+                <label>New Email:
+                </label>
+                <input
+                    disabled={adminAccount ? true : !allowEdit}
+                    type="email"
+                    value={adminAccount ? "Cannot Edit Test User" : verifyEmail.email}
+                    name="email"
+                    onChange={handleEmailChange}
+                />
+                <label>Confirm Email:
+                </label>
+                <input
+                    disabled={adminAccount ? true : !allowEdit}
+                    type="email"
+                    value={adminAccount ? "Cannot Edit Test User" : verifyEmail.confirmEmail}
+                    name="confirmEmail"
+                    onChange={handleEmailChange}
+                />
+                <button
+                    className="btn no-margin-left"
+                    disabled={disableEmailBtn}
+                >Update</button>
+            </fieldset>
         </form>
     )
 }
--- src/components/UserProfile/UserUpdatePassword.jsx
+++ src/components/UserProfile/UserUpdatePassword.jsx
@@ -1,46 +1,50 @@
 import React from 'react'
 
-export default function UserUpdatePassword({ disablePasswordBtn, tempUserData, handlePasswordUpdate, allowEdit, verifyPassword, handlePasswordChange, handlePasswordVerify, adminAccount,}) {
+export default function UserUpdatePassword({ disablePasswordBtn, tempUserData, handlePasswordUpdate, allowEdit, verifyPassword, handlePasswordChange, handlePasswordVerify, adminAccount, }) {
     return (
         <>
             <form onSubmit={handlePasswordVerify}>
-                Current Password:
-                <div>
+                <fieldset>
+                    Current Password:
+                    <div>
 
-                    <input
-                        disabled={adminAccount ? true : !allowEdit}
-                        type="password"
-                        value={adminAccount ? "Cannot Edit Test User" : verifyPassword.currentPassword}
-                        name="currentPassword"
-                        onChange={handlePasswordChange}
-                    />
-                    <button className="btn no-margin-top-bot">Check</button>
-                </div>
+                        <input
+                            disabled={adminAccount ? true : !allowEdit}
+                            type="password"
+                            value={adminAccount ? "Cannot Edit Test User" : verifyPassword.currentPassword}
+                            name="currentPassword"
+                            onChange={handlePasswordChange}
+                        />
+                        <button className="btn no-margin-top-bot">Check</button>
+                    </div>
+                </fieldset>
             </form>
             <form onSubmit={handlePasswordUpdate}>
-                <label>New Password:
-                </label>
-                <input
-                    disabled={adminAccount ? true :!verifyPassword.passwordMatch}
-                    type="text"
-                    value={verifyPassword.password}
-                    name="password"
-                    onChange={handlePasswordChange}
-                />
-                <label>Confirm Password:
-                </label>
-                <input
-                    disabled={adminAccount ? true :!verifyPassword.passwordMatch}
-                    type="text"
-                    value={verifyPassword.confirmPassword}
-                    name="confirmPassword"
-                    onChange={handlePasswordChange}
-                />
-                <p className="text-italic text-small">password must be at least 3 characters long</p>
-                <button
-                    className="btn no-margin-left"
-                    disabled={disablePasswordBtn}
-                >Update</button>
+                <fieldset>
+                    <label>New Password:
+                    </label>
+                    <input
+                        disabled={adminAccount ? true : !verifyPassword.passwordMatch}
+                        type="text"
+                        value={verifyPassword.password}
+                        name="password"
+                        onChange={handlePasswordChange}
+                    />
+                    <label>Confirm Password:
+                    </label>
+                    <input
+                        disabled={adminAccount ? true : !verifyPassword.passwordMatch}
+                        type="text"
+                        value={verifyPassword.confirmPassword}
+                        name="confirmPassword"
+                        onChange={handlePasswordChange}
+                    />
+                    <p className="text-italic text-small">password must be at least 3 characters long</p>
+                    <button
+                        className="btn no-margin-left"
+                        disabled={disablePasswordBtn}
+                    >Update</button>
+                </fieldset>
             </form>
         </>
     )
